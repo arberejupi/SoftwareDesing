e700363d9f5f6fa35d8bf8bd3b920d0eb6e29b65
granitlekaj <73529239+granitlekaj@users.noreply.github.com>
2024-01-19 22:53:01 +0100

    Add files via upload

---
+++ UniversitySystemManagement/Models/Enrollment.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityManagementSystem.Models
+{
+    public class Enrollment
+    {
+        [Key]
+        public int enrollment_id { get; set; }
+
+        public int student_id { get; set; }
+
+        public int course_id { get; set; }
+    }
+}
+
